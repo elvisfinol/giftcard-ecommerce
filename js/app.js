aa38f1dd80e77c57cea77ad74677d7000c2435d4
Elvis Finol <e.finol7@gmail.com>
2020-12-29 17:27:19 -0300

    json file para obtener producto e utilizacion de propiedad innerHTML para mostrar en HTML dinamicamente (productos)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,94 @@
+// ************************************************
+// Carrito de compra
+// ************************************************
+
+// Declaracion de variables
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('cart-total')
+const cartContent = document.querySelector('cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+// Carrito
+let cart = []
+
+// Clase para obtener productos
+// Las clases son "funciones especiales"
+// Los methods se crean dentro de las clases 
+class Products {
+    //Method
+    async getProducts() {
+        try {
+            let result = await fetch('./json/products.json')
+            let data = await result.json();
+
+            let products = data.items;
+            products = products.map(item => {
+                const {title,price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {title, price, id, image}
+            } )
+            return products
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+// Clase para mostrar producto desde "Products" o Local Storage
+// Desafio Clase 8 - Interacción con el DOM
+class UI {
+    displayProducts(products) {
+    let result = '';
+    products.forEach(product => {
+        result += `
+        <!-- single product -->
+            <article class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product" class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                        <i class="fas fa shopping-cart"></i>
+                        Agregar al carrito
+                    </button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>$${product.price}</h4>
+            </article>
+        <!--end single product-->
+        `
+    });
+    productsDOM.innerHTML = result;
+    }
+}
+
+// Clase de Storage Local
+class Storage {
+    static saveProducts(products) {
+        localStorage.setItem("products", JSON.stringify(products));
+    }
+}
+
+// Eventos - Desde aca traemos los objetos
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
+
+// Obteniendo todos los productos 
+    products.getProducts().then(products => {
+        ui.displayProducts(products);
+        Storage.saveProducts(products);
+    })
+})
+
+
+//----------------------------------------------------------------------------------------------------------//
+
+
 //////////////////////////// DESAFIO 4 //////////////////////////// 
 
 
@@ -74,21 +165,3 @@ function mensaje() {
 
     console.log(mensajeBienvenida.join()) // Output del array en formato string
 }
-
-// ************************************************
-// Shopping Cart API
-// ************************************************
-
-// Declaracion de variables
-const cartBtn = document.querySelector('.cart-btn');
-const closeCartBtn = document.querySelector('.close-cart');
-const clearCartBtn = document.querySelector('clear-cart');
-const cartDOM = document.querySelector('.cart');
-const cartOverlay = document.querySelector('cart-overlay');
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('cart-total')
-const cartContent = document.querySelector('cart-content');
-const productsDOM = document.querySelector('.products-center');
-
-// Carrito
-let cart = []
