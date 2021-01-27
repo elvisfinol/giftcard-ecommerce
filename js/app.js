eb8b50998653aab7ee833d232bf68898ba1e1201
Elvis Finol <e.finol7@gmail.com>
2021-01-26 23:12:39 -0300

    changing selectors

--- js/app.js
+++ js/app.js
@@ -1,153 +1,231 @@
-// ************************************************
-// Carrito de compra
-// ************************************************
+// Selectors
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+// Main cart
+let cart = [];
 
-// Declaracion de variables
-const carritoBtn = document.querySelector('.carrito-btn');
-const cierreCarritoBtn = document.querySelector('.cierre-carrito');
-const limpiarCarritoBtn = document.querySelector('limpiar-carrito');
-const carritoDOM = document.querySelector('.carrito');
-const carritoOverlay = document.querySelector('carrito-overlay');
-const carritoItems = document.querySelector('.carrito-items');
-const carritoTotal = document.querySelector('.carrito-total');
-const carritoContenido = document.querySelector('carrito-contenido');
-const productosDOM = document.querySelector('.productos-center');
+// buttons
+let buttonsDOM = [];
 
-// Carrito
-let carrito = [];
-// botones
-let botonesDOM = [];
+// Getting the products
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch('./json/products.json');
+      let data = await result.json();
+      let products = data.items;
 
-// Clase creada para para obtener productos con la fetch API al archivo JSON.
-// Las clases son "funciones especiales"
-// Los methods se crean dentro de las clases
-class Productos {
-    async obtenerProductos(){
-        try {
-            let resultado = await fetch('./json/products.json')
-            let data = await resultado.json();
-            
-            let productos = data.items;
-            productos = productos.map(item => {
-                const {title, price} = item.fields;
-                const {id} = item.sys;
-                const image = item.fields.image.fields.file.url;
-                return {title, price, image}
-            })
-            return productos;
-        } catch (error) {
-            console.log(error);
-        }
+      products = products.map(item => {
+        const {
+          title,
+          price
+        } = item.fields;
+        const {
+          id
+        } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return {
+          title,
+          price,
+          id,
+          image
+        };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
-// Pintar productos en HTML
+// display products
 class UI {
-    mostrarProductos(productos) {
-    let resultado = '';
-    productos.forEach(producto => {
-        resultado += `
-            <article class="producto">
-                <div class="img-container">
-                    <img src=${producto.image} alt="product" class="product-img">
-                    <button class="bag-btn" data-id=${producto.id}>
-                        <i class="fas fa shopping-cart"></i>
-                        Comprar ✋
-                    </button>
-                </div>
-                <h3>${producto.title}</h3>
-                <h4>$${producto.price}</h4>
-            </article>
-        `
+  displayProducts(products) {
+    let result = '';
+    products.forEach(product => {
+      result += `<article class="product">
+          <div class="img-container">
+            <img src=${product.image} alt="product" class="product-img">
+            <button class="bag-btn" data-id=${product.id}>
+              Comprar ✋
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article> `;
     });
-    productosDOM.innerHTML = resultado;
+    productsDOM.innerHTML = result;
+  }
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll('.bag-btn')];
+    buttonsDOM = buttons;
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+      if (inCart) {
+        button.innerText = 'En el carrito';
+        button.disabled = true;
+      }
+      button.addEventListener('click', (e) => {
+        e.target.innerText = 'En el carrito';
+        e.target.disabled = true;
+        // get product from products
+        let cartItem = {
+          ...Storage.getProduct(id),
+          amount: 1
+        };
+        console.log(cartItem);
+        // add product to cart
+        cart = [...cart, cartItem];
+        // save cart in local storage
+        Storage.saveCart(cart);
+        // set cart values 
+        this.setCartValues(cart);
+        // display cart item
+        this.addCartItem(cartItem);
+        // show cart
+        this.showCart();
+      });
+    });
+  }
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+  }
+  addCartItem(item) {
+    const div = document.createElement('div');
+    div.classList.add('cart-item');
+    div.innerHTML = `<img src=${item.image} alt="product">
+            <div>
+              <h4>${item.title}</h4>
+              <h5>$${item.price}</h5>
+              <span class="remove-item" data-id=${item.id}>remove item</span>
+            </div>
+            <div>
+              <i class="fas fa-chevron-up" data-id=${item.id}></i>
+              <p class="item-amount">${item.amount}</p>
+              <i class="fas fa-chevron-down" data-id=${item.id}></i>
+            </div>`;
+    cartContent.appendChild(div);
+  }
+  showCart() {
+    cartOverlay.classList.add('transparentBcg');
+    cartDOM.classList.add('showCart');
+  };
+  setupAPP() {
+    cart = Storage.getCart();
+    this.setCartValues(cart);
+    this.populateCart(cart);
+    cartBtn.addEventListener('click', this.showCart);
+    closeCartBtn.addEventListener('click', this.hideCart)
+  }
+  populateCart(cart) {
+    cart.forEach(item => this.addCartItem(item));
+  };
+  hideCart() {
+    cartOverlay.classList.remove('transparentBcg');
+    cartDOM.classList.remove('showCart');
+  };
+  cartLogic() {
+
+    // clear cart button
+    clearCartBtn.addEventListener('click', () => {
+      this.clearCart();
+    });
+
+    // cart functionality
+    cartContent.addEventListener('click', (e) => {
+      if (e.target.classList.contains('remove-item')) {
+        let removeItem = e.target;
+        let id = moveItem.dataset.id;
+        cartContent.removeChild(removeItem.parentElement.parentElement);
+        this.removeItem(id);
+      } else if (e.target.classList.contains('fa-chevron-up')) {
+        let addAmount = e.target;
+        let id = addAmount.dataset.id;
+        let tempItem = cart.find(item => item.id === id);
+        tempItem.amount = tempItem.amount + 1;
+        Storage.saveCart(cart);
+        this.setCartValues(cart);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
+        tempItem.amount;
+      } else if (e.target.classList.contains('fa-chevron-down')) {
+        let lowerAmount = e.target;
+        let id = lowerAmount.dataset.id;
+        let tempItem = cart.find(item => item.id === id);
+        tempItem.amount = tempItem.amount - 1;
+        if (tempItem.amount > 0) {
+          Storage.saveCart(cart);
+          this.setCartValues(cart);
+          lowerAmount.previousElementSibling.innerText = tempItem.amount;
+        } else {
+          cartContent.removeChild(lowerAmount.parentElement.parentElement);
+          this.removeItem(id);
+        }
+      }
+    });
+  };
+  clearCart() {
+    let cartItems = cart.map(item => item.id);
+    cartItems.forEach(id => this.removeItem(id));
+    while (cartContent.children.length > 0) {
+      cartContent.removeChild(cartContent.children[0]);
     }
-    bagBoton(){
-        let buttons = [...document.querySelectorAll(".bag-btn")];
-        botonesDOM = buttons;
-        buttons.forEach(button => {
-            let id = button.dataset.id;
-            let enCarrito = carrito.find(item => item.id === id);
-
-            if(enCarrito) {
-                button.innerText = "En el carrito";
-                button.disabled = true;
-            }
-            // Desafio Clase 9 - Incorporar eventos
-                button.addEventListener('click', (event) => {
-                    // Deshabilitar boton
-                    event.target.innerText = "En el carrito";
-                    event.target.disabled = true;
-                    // Metodos a crear
-                    // Obtener producto desde Productos
-                    let carritoItems = {...Storage.obtenerProducto(id), amount:1};
-                    // Agregar producto al carrito
-                    carrito = [...carrito,carritoItems];
-                    // Guardar carrito en el local storage
-                    Storage.guardarCarrito(carrito);
-                    // Setear valores del carrito 
-                    this.seteoCarritoValores(carrito);
-                    // Mostrar los items del carrito
-                    this.agregarCarritoItem(carritoItems);
-                    // Mostrar el carrito
-                });
-            });
-        }
-        seteoCarritoValores(carrito) {
-            let tempTotal = 0;
-            let itemsTotal = 0;
-            carrito.map(item => {
-                tempTotal += item.price * item.amount;
-                itemsTotal += item.amount;
-            });
-            carritoTotal.innerText = parseFloat(tempTotal.toFixed(2));
-            carritoItems.innerText = itemsTotal;
-        }
-        agregarCarritoItem(item) {
-            const div = document.createElement('div');
-            div.classList.add('carrito-items');
-            div.innerHTML = `<img src="/images/products/spotify-gift-card.jpg" alt="">
-                    <div>
-                        <h4>Spotify Card</h4>
-                        <h5>$ 10 </h5>
-                        <span class="remove-item">remove</span>
-                    </div>
-                    <div>
-                        <i class="fas fa-chevron-up"></i>
-                        <p class="item-amount">2</p>
-                        <i class="fas fa-chevron-down"></i>
-                    </div>`
-        }
-    }
-
-
-// Local Storage
-// Storage Method. Usando un metodo Static no tengo que llamar la instancia de la clase
+    this.hideCart();
+  };
+  removeItem(id) {
+    cart = cart.filter(item => item.id !== id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = `</i>Comprar ✋`;
+  }
+  getSingleButton(id) {
+    return buttonsDOM.find(button => button.dataset.id === id);
+  }
+}
+// local storage
 class Storage {
-    static guardarProductos(productos) {
-        localStorage.setItem("productos", JSON.stringify(productos));
-    }
-    static obtenerProducto(id) {
-        let productos = JSON.parse(localStorage.getItem('productos'));
-        return productos.find(producto => producto.id === id);
-    }
-        static guardarCarrito(carrito) {
-        localStorage.setItem('carrito', JSON.stringify(carrito));
-    }
+  static saveProducts(products) {
+    localStorage.setItem('products', JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem('products'));
+    return products.find(product => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+  static getCart() {
+    return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+  };
 }
 
-// EventListener
-// Desde aca llamamos a los metodos
-// Nota: no se incluye instancia de Storage class para usar Static Method
-document.addEventListener("DOMContentLoaded", () => {
-    const ui = new UI();
-    const productos = new Productos();
-
-    // Obtener productos 
-    productos.obtenerProductos().then(productos => {
-    ui.mostrarProductos(productos);
-    Storage.guardarProductos(productos);
-    }).then(() => {
-        ui.bagBoton();
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI();
+  const products = new Products();
+  // setup app
+  ui.setupAPP();
+  // get all products
+  products.getProducts().then(products => {
+    ui.displayProducts(products);
+    Storage.saveProducts(products);
+  }).then(() => {
+    ui.getBagButtons();
+    ui.cartLogic();
+  });
 });
